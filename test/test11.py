2916e4e95bebae70346fb8c9fab7f335184b6aff
박아연 <115124708+zincum30@users.noreply.github.com>
2023-09-08 13:46:23 +0900

    problem solving
    with python

---
+++ test/test11.py
@@ -0,0 +1,7 @@
+x, y = map(int, input().split())
+day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+month = ['1','2','3','4','5','6','7','8','9','10','11','12']
+
+
+
+
---
+++ test/test8.py
@@ -0,0 +1,3 @@
+N = int(input())
+test = [x for x in input()]
+
